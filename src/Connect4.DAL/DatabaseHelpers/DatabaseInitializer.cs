c9061f92db334f929ef6ef618be69d1a5adff496
Ivan Mahút <ivan.mahut.92@gmail.com>
2023-11-05 01:01:56 +0100

    Achievements and Join entity added
    DAL update + achievements, join
    BL update + service for achiev, join

--- src/Connect4.DAL/DatabaseHelpers/DatabaseInitializer.cs
+++ src/Connect4.DAL/DatabaseHelpers/DatabaseInitializer.cs
@@ -1,10 +1,12 @@
-﻿using Connect4.DAL.Repositories;
+﻿using Connect4.DAL.DataModels;
+using Connect4.DAL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Connect4.DAL.DatabaseHelpers
 {
@@ -23,7 +25,21 @@ namespace Connect4.DAL.DatabaseHelpers
                 Username TEXT NOT NULL,
                 Password TEXT NOT NULL,
                 Email TEXT NOT NULL UNIQUE
-            )";
+            );
+            
+            CREATE TABLE IF NOT EXISTS Achievements(
+                AchievementId INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Description TEXT
+            );
+                
+            CREATE TABLE IF NOT EXISTS UserAchievements(
+                UserAchievementId INTEGER PRIMARY KEY AUTOINCREMENT,
+                UserId INTEGER,
+                AchievementId INTEGER,
+                Earned BOOLEAN
+            );
+                ";
                 using (var command = new SQLiteCommand(createTableQuery, connection))
                 {
                     command.ExecuteNonQuery();
@@ -44,7 +60,27 @@ namespace Connect4.DAL.DatabaseHelpers
         INSERT INTO Users (Username, Password, Email) VALUES 
         ('DummyUser1', 'DummyPass1', 'dummy1@email.com'),
         ('DummyUser2', 'DummyPass2', 'dummy2@email.com'),
-        ('DummyUser3', 'DummyPass3', 'dummy3@email.com')";
+        ('DummyUser3', 'DummyPass3', 'dummy3@email.com');
+
+        INSERT INTO UserAchievements (Name, Description, Earned) VALUES
+        ('Getting Started!', 'Play one game of Connect4.'),
+        ('Played 10!','Play ten games of Connect4.'),
+        ('Played 100!','Play hundred games of Connect4.'),
+        ('Played 1000!','Play thousand games of Connect4.'),
+        ('THE LEGEND!!!','Play ten thousand games of Connect4.'),
+        ('4-in-a-Row!','Win in first 4 turns.'),
+        ('No rookie anymore!','Win fifty games of Connect4!'),
+        ('Connect4 Veteran!','Win five hundred games of Connect4.'),
+        ('Every token counts!','Win by placing the last token of the game.'),
+        ('The Ultimate Collector!','Collect all cosmetics.');
+
+        INSERT INTO UserAchievements (UserAchievementId, UserId, AchievementId, Earned) VALUES
+         (1, 1, 1, 1),
+         (2, 1, 2, 0),
+         (3, 2, 1, 1),
+         (4, 2, 2, 1),
+         (5, 2, 3, 0);
+        ";
 
             using (var command = new SQLiteCommand(insertDummyQuery, connection))
             {
---
+++ src/Connect4.DAL/Repositories/AchievementRepository.cs
@@ -0,0 +1,79 @@
+﻿using Connect4.DAL.DatabaseHelpers;
+using Connect4.DAL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Connect4.DAL.Repositories
+{
+    public class AchievementRepository
+    {
+        public List<Achievement> GetAllAchievements()
+        {
+            List<Achievement> achievements = new List<Achievement>();
+
+            using (var connection = DatabaseConnection.GetConnection())
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Achievements";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            achievements.Add(new Achievement
+                            {
+                                AchievementId = Convert.ToInt32(reader["AchievementId"]),
+                                Name = reader["Name"].ToString(),
+                                Description = reader["Description"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            return achievements;
+        }
+
+        public Achievement GetAchievementById(int achievementId)
+        {
+            Achievement achievement = null;
+
+            string query = "SELECT * FROM Achievements WHERE AchievementId = @AchievementId";
+
+            using (var connection = DatabaseConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("AchievementId", achievementId);
+
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            achievement = new Achievement
+                            {
+                                AchievementId = Convert.ToInt32(reader["AchievementId"]),
+                                Name = reader["Name"].ToString(),
+                                Description = reader["Description"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+
+            return achievement;
+        }
+    }
+}
