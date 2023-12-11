9d49b8c77fc608162ffca07ea156b2b9a8101213
xmajer25 <101834020+xmajer25@users.noreply.github.com>
2023-12-11 03:46:00 +0100

    basic view to view model logic for crazy house

--- src/Connect4/ViewModel/CrazyHouseViewModel.cs
+++ src/Connect4/ViewModel/CrazyHouseViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Connect4.Commands;
+﻿using Connect4.BL.Services.GridServices;
+using Connect4.Commands;
 using Connect4.DAL.DatabaseHelpers;
 using Connect4.DAL.DataModels;
 using Connect4.ViewModel.Interfaces;
+using Connect4.ViewUserControl;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 using NavService = Connect4.Services.NavigationService;
 
 namespace Connect4.ViewModel
@@ -12,7 +21,13 @@ namespace Connect4.ViewModel
     {
         public User CurrentUser { get; set; }
         private readonly NavService _navigationService;
+
+        public Grid MainGrid;
+        public Grid TopGrid;
+        public Canvas TopCanvas;
         public ICommand NavigateToPickVariantCommand { get; private set; }
+        public ICommand DropBallCommand { get; private set; }
+        public GridCrazyHouseService GridService => new GridCrazyHouseService();
 
         public CrazyHouseViewModel()
         {
@@ -20,7 +35,50 @@ namespace Connect4.ViewModel
             _navigationService = new NavService();
 
             NavigateToPickVariantCommand = new RelayCommand<object>(NavigateToPickVariant);
+            DropBallCommand = new RelayCommand<int>(DropBall);
         }
+
+        public double GetColumnPosition(int index)
+        {
+            double columnXPosition = 0;
+
+            for (int i = 0; i < index; i++)
+            {
+                columnXPosition += TopGrid.ColumnDefinitions[i].ActualWidth;
+            }
+            return columnXPosition;
+        }
+
+        public void DropBall(int column)
+        {
+            BallControl ball = new BallControl();
+            
+            Canvas.SetLeft(ball, GetColumnPosition(column));  // Set the X position based on the column
+            Canvas.SetTop(ball, 0);  // Set the initial Y position
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                To = TopCanvas.ActualHeight,
+                Duration = TimeSpan.FromSeconds(3),
+                FillBehavior = FillBehavior.Stop
+            };
+
+            ball.Visibility = Visibility.Visible;
+
+            Storyboard.SetTarget(animation, ball);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.TopProperty));
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+
+            storyboard.Completed += (sender, e) =>
+            {
+                TopCanvas.Children.Remove(ball);
+            };
+
+            TopCanvas.Children.Add(ball);
+            storyboard.Begin();
+        }
+
         public void LoadUser(User user)
         {
             CurrentUser = user;
--- src/Connect4/Views/CrazyHouseView.xaml.cs
+++ src/Connect4/Views/CrazyHouseView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Connect4.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,39 @@ namespace Connect4.Views
         public CrazyHouseView()
         {
             InitializeComponent();
+            CrazyHouseViewModel viewModel = new CrazyHouseViewModel();
+            viewModel.MainGrid = MainGrid;
+            viewModel.TopGrid = TopGrid;
+            viewModel.TopCanvas = TopCanvas;
+
+            DataContext = viewModel;
+        }
+        private void ColumnButton1_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 1);
+        private void ColumnButton2_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 2);
+        private void ColumnButton3_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 3);
+        private void ColumnButton4_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 4);
+        private void ColumnButton5_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 5);
+        private void ColumnButton6_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 6);
+        private void ColumnButton7_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 7);
+        private void ColumnButton8_Click(object sender, RoutedEventArgs e)
+            => StartAnimation(sender, e, 8);
+
+        private void StartAnimation(object sender, RoutedEventArgs e, int column)
+        {
+            if (sender is Button)
+            {
+                if (DataContext is CrazyHouseViewModel viewModel)
+                {
+                    viewModel.DropBallCommand.Execute(column); 
+                }
+            }
         }
     }
 }
